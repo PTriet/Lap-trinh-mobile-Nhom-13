9ddcc8f3741dba257d1a2649a2f247e73173d0bb
PhanDinhThanh2004 <duonglanh.20071978@gmail.com>
2025-04-14 19:43:07 +0700

    Thêm tài nguyên hình ảnh và cập nhật giao diện, cấu hình dự án

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,34 +40,37 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    implementation(platform(libs.androidx.compose.bom))
+    // Compose
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
 
+    // AndroidX View system
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.material)
-    implementation(libs.androidx.material.icons.extended) // ✅ Thêm đúng alias này
-
     implementation(libs.constraintlayout)
-    implementation(libs.androidx.material3)
 
+    // Testing
+    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
+    // Debug
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
